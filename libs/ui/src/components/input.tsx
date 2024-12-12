b1913d7d783934750002f94a42cf3d780c322de6
Tuan <batuan152@gmail.com>
2024-12-12 16:48:25 +0700

    fix(button)

--- libs/ui/src/components/input.tsx
+++ libs/ui/src/components/input.tsx
@@ -14,7 +14,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       // eslint-disable-next-line tailwindcss/no-custom-classname
       className={cn(
         "flex h-9 w-full rounded border border-border bg-[inherit] px-3 py-0.5 !text-sm ring-0 ring-offset-transparent transition-colors [appearance:textfield] placeholder:opacity-80 hover:bg-background focus:border-primary focus:bg-secondary/20 focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none",
-        "file-input file:border-0 file:bg-transparent file:pt-1 file:text-sm file:font-medium",
+        "file-input file:border-0 file:pt-1 file:text-sm file:font-medium",
         hasError ? "border-error" : "border-border",
         className,
       )}
