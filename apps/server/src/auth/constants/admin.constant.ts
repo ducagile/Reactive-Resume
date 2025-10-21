b58ce446ffc7e8b2c99080b11cacd64ccf8b01cd
Tuan <batuan152@gmail.com>
2025-10-21 15:17:15 +0700

    fix: admin login

--- apps/server/src/auth/constants/admin.constant.ts
+++ apps/server/src/auth/constants/admin.constant.ts
@@ -1,1 +1,5 @@
 export const ROLE_KEY = "role";
+export const ADMIN_RT = "Admin-Refresh";
+export const ADMIN_AT = "Admin-Authentication";
+export const AT = "Authentication";
+export const RT = "Refresh";
